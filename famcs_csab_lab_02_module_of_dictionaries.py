f1b773bb6a66e8ed354c2a08ba984067e2c88aa4
vetasavitskaya <veta.s.working@gmail.com>
2021-12-01 23:03:06 +0300

    Update README.md

--- famcs_csab_lab_02_module_of_dictionaries.py
+++ famcs_csab_lab_02_module_of_dictionaries.py
@@ -1,16 +1,29 @@
 import sys
-from PyQt5 import QtWidgets
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QTableWidget, QHeaderView, QVBoxLayout, QLineEdit, \
-    QPushButton, QLabel, QHBoxLayout, QFileDialog, QTableWidgetItem, QCheckBox, QComboBox
+    QPushButton, QLabel, QHBoxLayout, QTableWidgetItem, QComboBox, QPlainTextEdit
+
+
+class AddEditWindow(QWidget):
+    def __init__(self, window_title):
+        super().__init__()
+        self.setWindowTitle(window_title)
+        self.entry_form_layout = QVBoxLayout()
+        self.setFixedSize(230, 400)
+        self.setLayout(self.entry_form_layout)
 
 
 class ContentWindow(QWidget):
     def __init__(self):
         super().__init__()
+
+        self.add_or_edit_renter_window = None
+        self.add_or_edit_rent_contract_window = None
+
         self.dictionary_data = []
         self.current_dictionary_name = "no dictionary is selected"
         self.number_of_columns_in_table = 0
+        self.new_row_to_insert = []
 
         self.dictionaries_headers = {'renters': ['name', 'address', 'passport id', "driver's license"],
                                      'rent contracts': ['renter name', 'car number', 'rent starting date',
@@ -38,32 +51,11 @@ class ContentWindow(QWidget):
         self.button_edit_row = QPushButton('edit row')  # выход
         self.button_edit_row.setFixedSize(200, 28)
 
-        self.button_enter = QPushButton('enter')  # выход
-        self.button_enter.setFixedSize(200, 28)
-
         self.button_quit = QPushButton('exit')  # выход
         self.button_quit.setFixedSize(200, 28)
 
-        self.renter_name = QLineEdit()
-        self.renter_name.setFixedSize(200, 28)
-
-        self.renter_surname = QLineEdit()
-        self.renter_surname.setFixedSize(200, 28)
-
-        self.renter_patronymic = QLineEdit()
-        self.renter_patronymic.setFixedSize(200, 28)
-
-        self.renter_address = QLineEdit()
-        self.renter_address.setFixedSize(200, 28)
-
-        self.renter_passport_id = QLineEdit()
-        self.renter_passport_id.setFixedSize(200, 28)
-
-        self.renter_drivers_license_number = QLineEdit()
-        self.renter_drivers_license_number.setFixedSize(200, 28)
-
         self.enter_error = QLineEdit()  # вывод ошибок
-        self.set_base_input_form_layout()
+
         self.layout = QHBoxLayout()
         self.layout.addWidget(self.dictionary_layout)
         self.layout.setAlignment(self.dictionary_layout, Qt.AlignTop)
@@ -71,11 +63,9 @@ class ContentWindow(QWidget):
         self.layout.setAlignment(self.entry_form_layout, Qt.AlignTop)
         self.setLayout(self.layout)
         self.combo.activated[str].connect(self.choose_dictionary)
-        self.button_add_row.clicked.connect(self.add_row)
+        self.button_add_row.clicked.connect(self.show_add_or_edit_renter_window)
         self.button_quit.clicked.connect(lambda: application_window.quit())
-        # self.bank_code.textChanged[str].connect(self.check_disable)
 
-    def set_base_input_form_layout(self):
         self.enter_error.setFixedSize(200, 28)
         self.enter_error.setText("No errors yet !")
         self.enter_error.setEnabled(False)
@@ -85,25 +75,6 @@ class ContentWindow(QWidget):
         self.entry_form_layout.addWidget(self.button_add_row)
         self.entry_form_layout.addWidget(self.button_edit_row)
         self.entry_form_layout.addWidget(self.button_quit)
-
-    def set_add_input_form_layout(self):
-        self.set_base_input_form_layout()
-        self.entry_form_layout.addWidget(QLabel('renter name :'))
-        self.entry_form_layout.addWidget(self.renter_name)
-
-        self.entry_form_layout.addWidget(QLabel('renter patronymic:'))
-        self.entry_form_layout.addWidget(self.renter_patronymic)
-
-        self.entry_form_layout.addWidget(QLabel('renter address :'))
-        self.entry_form_layout.addWidget(self.renter_address)
-
-        self.entry_form_layout.addWidget(QLabel('renter passport id :'))
-        self.entry_form_layout.addWidget(self.renter_passport_id)
-
-        self.entry_form_layout.addWidget(QLabel('renter drivers license_number :'))
-        self.entry_form_layout.addWidget(self.renter_drivers_license_number)
-
-        self.entry_form_layout.addWidget(self.button_enter)
 
     def choose_dictionary(self, dictionary_name):
         self.current_dictionary_name = dictionary_name
@@ -130,20 +101,72 @@ class ContentWindow(QWidget):
         self.dictionary_layout.setAlternatingRowColors(True)
 
     def check_disable(self):
-        if self.invoice_number.text() and self.payment_reference.text() and self.account_number.text() \
-                and self.bank_code.text() and self.is_any_template_selected and self.items > 0:
+        if self.add_or_edit_renter_window.renter_name.text() and self.add_or_edit_renter_window.renter_surname.text() \
+                and self.add_or_edit_renter_window.renter_patronymic \
+                and self.add_or_edit_renter_window.renter_address.text() \
+                and self.add_or_edit_renter_window.renter_passport_id.text() \
+                and self.add_or_edit_renter_window.renter_drivers_license_number.text():
             self.button_save_to_word.setEnabled(True)
         else:
             self.button_save_to_word.setEnabled(False)
 
     def add_row(self):
-        self.set_add_input_form_layout()
+        self.dictionary_layout = self.entry_form_layout
+
+    def show_add_or_edit_renter_window(self):
+        self.dictionary_layout = self.entry_form_layout
+
+    def show_add_or_edit_rent_contract_window(self):
+
+        if self.add_or_edit_renter_window is None:
+            self.add_or_edit_renter_window = AddEditWindow("Add New Renter")
+
+            self.add_or_edit_renter_window.renter_name = QLineEdit()
+            self.add_or_edit_renter_window.renter_name.setFixedSize(200, 28)
+
+            self.add_or_edit_renter_window.renter_surname = QLineEdit()
+            self.add_or_edit_renter_window.renter_surname.setFixedSize(200, 28)
+
+            self.add_or_edit_renter_window.renter_patronymic = QLineEdit()
+            self.add_or_edit_renter_window.renter_patronymic.setFixedSize(200, 28)
+
+            self.add_or_edit_renter_window.renter_address = QPlainTextEdit()
+            self.add_or_edit_renter_window.renter_address.setFixedSize(200, 48)
+
+            self.add_or_edit_renter_window.renter_passport_id = QLineEdit()
+            self.add_or_edit_renter_window.renter_passport_id.setFixedSize(200, 28)
+
+            self.add_or_edit_renter_window.renter_drivers_license_number = QLineEdit()
+            self.add_or_edit_renter_window.renter_drivers_license_number.setFixedSize(200, 28)
+
+            self.add_or_edit_renter_window.entry_form_layout.setSpacing(9)
+            self.add_or_edit_renter_window.entry_form_layout.addWidget(QLabel('name :'))
+            self.add_or_edit_renter_window.entry_form_layout.addWidget(self.add_or_edit_renter_window.renter_name)
+
+            self.add_or_edit_renter_window.entry_form_layout.addWidget(QLabel('patronymic:'))
+            self.add_or_edit_renter_window.entry_form_layout.addWidget(self.add_or_edit_renter_window.renter_patronymic)
+
+            self.add_or_edit_renter_window.entry_form_layout.addWidget(QLabel('address :'))
+            self.add_or_edit_renter_window.entry_form_layout.addWidget(self.add_or_edit_renter_window.renter_address)
+
+            self.add_or_edit_renter_window.entry_form_layout.addWidget(QLabel('passport id :'))
+            self.add_or_edit_renter_window.entry_form_layout.addWidget(
+                self.add_or_edit_renter_window.renter_passport_id)
+
+            self.add_or_edit_renter_window.entry_form_layout.addWidget(QLabel('drivers license_number :'))
+            self.add_or_edit_renter_window.entry_form_layout.addWidget(
+                self.add_or_edit_renter_window.renter_drivers_license_number)
+
+            self.add_or_edit_renter_window.button_enter = QPushButton('enter')
+            self.add_or_edit_renter_window.button_enter.setFixedSize(200, 28)
+            self.add_or_edit_renter_window.entry_form_layout.addWidget(self.add_or_edit_renter_window.button_enter)
+            # self.bank_code.textChanged[str].connect(self.check_disable)
+        self.add_or_edit_renter_window.show()
 
 
 class MainWindow(QMainWindow):  # создали окошко приложения
     def __init__(self, window_content):
         super().__init__()
-        #  Добавить на форму ваши данные: ФИО, год, курс, группа.
         self.setWindowTitle("Савицкая Елизавета Дмитриевна, 2021 год, 4 курс, 4 группа")
         self.setFixedSize(1080, 800)
         self.setCentralWidget(window_content)
---
+++ test.py
@@ -0,0 +1,4 @@
+res ='{0:,}'.format(10000).replace(',', ' ')
+print(res)
+
+res ='{0:,}'.format(res).replace(',', ' ')
